88b3048096cd15b8656f2dd85af0330adb83972f
Krasimir Chariyski <krasimir.chariyski@sap.com>
2015-09-11 09:14:05 +0300

    [INTERNAL] Added travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 
 module.exports = function (config) {
-    config.set({
+    var configuration = {
 
         // Base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: './',
@@ -92,8 +92,21 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
 
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false
-    });
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };

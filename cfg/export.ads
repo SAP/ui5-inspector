335ee0ae0f3e4c69c2d7eec0bf4b3a0c43bede64
Krasimir Chariyski <krasimir.chariyski@sap.com>
2015-11-12 10:09:26 +0200

    [INTERNAL] Remove xMake files

--- cfg/export.ads
+++
@@ -1,7 +0,0 @@
-artifacts builderVersion:"1.0", {
-    group "com.sap.ui5inspector", {
-        artifact "package", {
-            file "${gendir}/module/package/ui5inspector.zip", extension: "zip"
-        }
-    }
-}

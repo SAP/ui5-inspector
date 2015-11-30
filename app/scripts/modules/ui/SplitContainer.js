13cb2c7afcd14db201f2d17cb33898d3accf1628
Marin Parushev <marin.parushev@sap.com>
2015-11-30 11:52:34 +0200

    [INTERNAL] Pull request test

--- app/scripts/modules/ui/SplitContainer.js
+++ app/scripts/modules/ui/SplitContainer.js
@@ -219,6 +219,7 @@ SplitContainer.prototype._mouseMoveHandler = function (event) {
  */
 SplitContainer.prototype._mouseUpHandler = function () {
     this.$this.onmousemove = null;
+    // TODO.
     document.body.className = '';
 };
 
